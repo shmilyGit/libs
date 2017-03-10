51c7d4e47d85c72e3da92fe8b00961cdafc7716a
root <root@UTM2011.(none)>
2017-03-10 10:21:50 +0800

    20170310:linj:add public.h

---
+++ public.h
@@ -0,0 +1,15 @@
+#ifndef __PUBLIC_H
+#define __PUBLIC_H
+
+#include <sys/socket.h>   
+#include <sys/types.h>
+#include <stdlib.h>   
+#include <stdio.h>   
+#include <string.h>   
+#include <arpa/inet.h>   
+#include <unistd.h>   
+#include <sys/ioctl.h>   
+#include <linux/if_arp.h>   
+//#include <glib.h>
+
+#endif
